82eeccd359c0784dbe510969ad56dd5f85bb11a6
victim0596 <81644467+victim0596@users.noreply.github.com>
2024-08-07 19:42:07 +0200

    Added logic for handling

--- generatorLine/genHandling.cs
+++ generatorLine/genHandling.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,9 +10,22 @@ namespace addVehicle.generatorLinee
 {
     public class genHandling
     {
-        public Task<Generator> start()
+        public Task<Generator> start(Info info)
         {
-            return Task.FromResult(new Generator());
+            string path = $"{info.pathGta}\\data\\handling.cfg";
+            Generator generator = new Generator();
+            var lines = File.ReadAllLines(path).ToArray();
+            for (var i = 0; i < lines.Length; i += 1)
+            {
+                var line = lines[i];
+                if(line.Contains(info.nameVehicleToCopy))
+                {
+                    generator.line = line;
+                    generator.result = true;
+                    break;
+                }
+            }
+            return Task.FromResult(generator);
         }
     }
 }
